47c6f18e5c667d379c222f71534bcd4b1fc1bba4
Aaron Murillo <amurillo11@cnm.edu>
2023-05-15 16:21:41 -0600

    adding courses

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,107 @@
+// let Js = "amazing";
+// // console.log(40 + 24 - 10);
+
+// console.log("Jonas");
+// console.log("24");
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// //Variable name conventions
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let Person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+// // const job;
+
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = "Schmedtmann";
+// console.log(lastName);
+
+// const ageAaron = 2037 - 1999;
+// const ageSarah = 2037 - 2023;
+// console.log(ageAaron, ageSarah);
+
+// let setTimeout(() => {
+//    ageAaron
+//    else() 
+   
+// }, timeout);
+
+
+// //Math operators
+// const now = 2037;
+// const ageAaron = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageAaron, ageSarah);
+
+// console.log(ageAaron * 2, ageAaron / 10, 2 ** 3)
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 
+
+// const firstName = "Aaron";
+// const lastName = 'Murillo';
+// console.log(firstName +' '+  lastName);
+
+// //Assignment operators
+// let x = 10 + 5; //15
+// x +=10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 
+// x--; // x + x - 1
+// x--;
+//  console.log(x);
+
+// // Comparison operators
+// console.log(ageAaron > ageSarah); //>, <, >=, <=
+// console.log(ageSarah >= 18)
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1911 > now - 2018)
+
+const now = 2037;
+const ageAaron = now - 1991;
+const ageSarah = now - 2018; 
+
+console.log (now - 1991 > now - 2018);
