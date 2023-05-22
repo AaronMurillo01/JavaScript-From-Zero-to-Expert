2cf92b44b1ba2abe69d08e41c7c3dd76d4e84291
Aaron Murillo <amurillo11@cnm.edu>
2023-05-22 11:43:10 -0600

    adding coding challenge #3 with answer

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -274,24 +274,101 @@
 // }
 
 
-const age = 18;
-if(age === 18) console.log('You just became an adult :D');
+// const age = 18;
+// if(age === 18) console.log('You just became an adult :D');
 
 
-if(age == 18) console.log('You just became an adult :D (loose)');
+// if(age == 18) console.log('You just became an adult :D (loose)');
 
-const favorite = Number (prompt("What's your favorite number"));
-console.log(favorite);
-console.log(typeof favorite);
+// const favorite = Number (prompt("What's your favorite number"));
+// console.log(favorite);
+// console.log(typeof favorite);
 
-if (favorite === 23) {
-    console.log('Cool! 23 is a amazing number!')
-} else if(favorite === 7) {
-    console.log('7 is also a cool number')
-} else if (favorite === 9) {
-    console.log('9 is also a cool number')
+// if (favorite === 23) {
+//     console.log('Cool! 23 is a amazing number!')
+// } else if(favorite === 7) {
+//     console.log('7 is also a cool number')
+// } else if (favorite === 9) {
+//     console.log('9 is also a cool number')
+// } else {
+//     console.log('Number is not 23 or 7 or 9')
+// }
+
+// if (favorite !== 23) console.log('Why not 23?');
+
+// const hasDriversLicense = true; // A B
+// const hasGoodVision = true;// B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// // if(hasDriversLicense && hasGoodVision) {
+// //     console.log('Sarah is able to drive!');
+// // } else {
+// //     console.log('Someone else should drive...');
+// // }
+
+// const isTired = false; //c
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+
+
+// Coding Challenge #3
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score).
+
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks 😉
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+// GOOD LUCK 😀
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3; 
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy')
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log('Koalas win the trophy')
+// } else if (scoreDolphins===scoreKoalas) {
+//     console.log('Both win the trophy!')
+// }
+
+// Bonus 1
+const scoreDolphins = (97 + 112 + 81) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3; 
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy')
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy')
+} else if (scoreDolphins===scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!')
 } else {
-    console.log('Number is not 23 or 7 or 9')
+    console.log('No one wins the trophy')
 }
-
-if (favorite !== 23) console.log('Why not 23?');
